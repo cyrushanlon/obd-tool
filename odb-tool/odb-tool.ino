7d18238762123f165d2f29fed4a9929353f432bf
Cyrus Hanlon <cyrushanlon@hotmail.com>
2018-09-25 18:24:08 +0100

    renamed file and changed to UART from i2c

--- odb-tool/odb-tool.ino
+++ odb-tool/odb-tool.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <OBD.h>
+#include <OBD2UART.h>
 
-COBDI2C obd;
+COBD obd;
 
 LiquidCrystal_I2C lcd(0x38, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address, if it's not working try 0x27.
 
