ae70cc1c82bef60add11cfb94e3101373c3a1c6d
Cyrus Hanlon <cyrushanlon@hotmail.com>
2019-01-29 23:44:22 +0000

    overhauled to improve FPS and reliability

--- odb-tool/odb-tool.ino
+++ odb-tool/odb-tool.ino
@@ -1,146 +1,139 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+//configuration
+#define DISPLAY_X 128
+#define DISPLAY_Y 56 //64
+#define TARGET_FPS 60 //hz
+#define SERIAL_ON
+//#define OBD_ON
+
+#include <U8g2lib.h>
+
+#ifdef OBD_ON
 #include <OBD2UART.h>
 
+COBD obd;
 
-//TODO
-//support short term averages (5 seconds for Fuel %)
-//support resetting averages
-//range
-//graphs
-//allow for different types of items
-//the below is a raw item
-//need support for sub items such as MPG
+int obdGetValue(int type) {
+  return 9000;
+}
 
-struct item {
-  String name; // name of the item
-  float current; // current value of the item
-  float max; // maximum value observed so far
-
-  float avg;
-  unsigned int avgCount;
-
-  int PID; // PID to collect
-  int dp; // number of decimal places to present the number with
-};
-
-//configuration
-const bool DEBUG = true;
-const int buttonPin = 2;
-const int targetFPS = 3; //3 hz
-const int modes = 3;
-item items[3] = {
-  {"RPM", 0, 0, 0, 0, PID_RPM, 0},
-  {"MPH", 0, 0, 0, 0, PID_SPEED, 0},
-  {"FUE", 0, 0, 0, 0, PID_FUEL_LEVEL, 0}
-};
+#endif
 
 //hardware
-COBD obd;
-LiquidCrystal_I2C lcd(0x38, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address, if it's not working try 0x27.
+U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, 10, 9, 8);
 
-// vars
-int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+int scaleValueToScreenY(float val, float min, float max) {
 
-unsigned long lastFrame;
-int mode;
+  // val = 5000
+  // min = 0    -> 0
+  // max = 9000 -> 64
 
-void doButton() {
+  return DISPLAY_Y - ((val / max) * DISPLAY_Y);
+}
 
-  // read the state of the switch
-  int reading = digitalRead(buttonPin);
+class item {
+public:
+  char* name;
+  float average;
+  float currentValue;
+  uint64_t  count;
 
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
+  int graph[DISPLAY_X];
+  
+  item() {
+    average = 0;
+    count = 0;
 
-  //if time since last bounce is longer than max delay we assume its a correct press
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        mode++;
-        if (mode > modes-1) mode = 0;
-      }
+    for (int i = 0; i < DISPLAY_X; i++) {
+      //this->graph[i] = DISPLAY_Y;
+      this->graph[i] = DISPLAY_Y * (1 + sin((float(i) / float(DISPLAY_X)) * 2 * M_PI)) / 2;
     }
   }
-
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;
-}
-
-void display() {
-  item i = items[mode];
-
-  lcd.clear();
-
-  lcd.setCursor(0, 0);
-  lcd.print(i.name + " CUR MAX AVG");
-  lcd.setCursor(0, 1);
-  lcd.print(i.current, i.dp);
-  lcd.print(" ");
-  lcd.print(i.max, i.dp);
-  lcd.print(" ");
-  lcd.print(i.avg, i.dp);
-}
-
-void setup() {
-  // init the button
-  pinMode(buttonPin, INPUT);
-  //init the LCD
-  lcd.begin(16, 2);
-  //lcd.backlight();
-  lcd.setCursor(0, 0);
-
-  if (!DEBUG) {
-    lcd.print("INITIALISING");
-    // start communication with OBD-II adapter
-    obd.begin();
-    while (!obd.init());
-  } else {
-    lcd.print("DEBUG MODE");
-    delay(1000);
-  }
-
-  lcd.clear();
-
-  //set the first lastFrame to avoid odd behaviour
-  lastFrame = micros();
-}
-
-void loop() {
-
-  //input
-  doButton();
-
-  //get values from obd
-  for (int i = 0; i < modes; i++) {
-  int val = 0;
-    if (!DEBUG && obd.readPID(items[i].PID, val)) {
-
-      items[i].current = val;
-      items[i].avgCount++;
-      items[i].avg = items[i].avg + ((val - items[i].avg) / items[i].avgCount);
-
-      if (items[i].max < val) {
-        items[i].max = val;
+  
+  void draw() {    
+    u8g2.firstPage();
+    do {
+      for (int x = 0; x < DISPLAY_X; x++) {
+        u8g2.drawPixel(x, this->graph[x]);  
       }
+  
+      u8g2.setCursor(0, 64);
+      u8g2.print(u8x8_u16toa(currentValue, 4)); //maybe cast to string?
+    } while ( u8g2.nextPage() );
+  }  
+
+  void addValue(float val) {
+    
+    count++;
+    this->average = this->average + ((val - this->average) / float(this->count));
+
+    // move all values back and reinsert 128
+    for (int i = 0; i < DISPLAY_X - 1; i++) {
+      this->graph[i] = this->graph[i + 1];
     }
+    
+    //add new value
+    this->graph[DISPLAY_X - 1] = scaleValueToScreenY(val, 0, 9000);
+    this->currentValue = val;
   }
+  
+};
+
+//holds item definitions
+item items[1] = {};
+
+//item that is currently displayed
+int currentItem = 0;
+
+void setup(void) {
+
+#ifdef SERIAL_ON
+  //connect to Serial monitor over USB
+  Serial.begin(9600);
+  Serial.println("START");
+#endif
+
+  //display init
+  u8g2.begin();
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_6x10_tn);
+    u8g2.drawStr(0, 0, "INITIALISING");
+  } while ( u8g2.nextPage() );
+
+  //config init
+  items[0] = item();
+  
+#ifdef OBD_ON
+  //obd init
+  obd.begin();
+  while (!obd.init()); 
+#endif
+}
+
+unsigned long lastFrame = millis();
+void loop(void) {
 
   //display if we arent going to overshoot the target fps
   unsigned long now = millis();
-  if (now - lastFrame > (1000 / targetFPS)) {
-    display();
+  if (now - lastFrame > (1000 / TARGET_FPS)) {
+
+#ifdef SERIAL_ON
+    Serial.println(now - lastFrame);
+#endif
+
+    int val = 0;
+
+#ifdef OBD_ON
+
+    val = obdGetValue(PID_RPM);
+    
+#endif
+
+    items[currentItem].addValue(random(9000));
+    
+    items[currentItem].draw();
+
     lastFrame = now;
   }
 }
+
