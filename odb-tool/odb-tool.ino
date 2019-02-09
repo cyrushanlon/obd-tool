b3ed5cdc953ef7dd1989401effdc55d9c7481866
Cyrus Hanlon <cyrushanlon@hotmail.com>
2019-02-09 13:00:12 +0000

    added optional aditional logic to allow for composite graphs

--- odb-tool/odb-tool.ino
+++ odb-tool/odb-tool.ino
@@ -7,10 +7,10 @@
 #define DISPLAY_X 320 - BAR_WIDTH
 #define GRAPH_Y 200 //64
 #define DISPLAY_Y 240 //64
-#define TARGET_FPS 1000 / 30 //hz
-#define ITEM_COUNT 4
+#define TARGET_FPS 1000 / 60 //hz
+#define ITEM_COUNT 7
 #define BG_COLOR ILI9341_BLACK
-//#define OBD_ON
+#define OBD_ON
 
 //hardware
 COBD obd;
@@ -23,20 +23,22 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 //#define TOUCH_TIRQ 2 //optional interupt pin
 XPT2046_Touchscreen ts(TOUCH_CS);//, TOUCH_TIRQ);
 
-
-int obdGetValue(int type) {
-  
-  int value;
-  if (obd.readPID(type, value)) {
-    return value;
-  }
-
-  return 0; //oh no
-}
-
 int scaleValueToScreenY(float val, float min, float max) {
 
   return (GRAPH_Y - 1) - ((val / max) * (GRAPH_Y - 1));
+}
+
+float kmhToMPH(int* pidVals) {
+  return float(pidVals[0]) * 0.621371;
+}
+
+float calcMPG(int* pidVals) {
+  //MPH
+  float speed = float(pidVals[0]) * 0.621371;
+  //GPH
+  float fuel = float(pidVals[1]) / 0.0805;
+  
+  return speed / fuel;
 }
 
 class item {
@@ -48,20 +50,46 @@ public:
   float minValue;
   float maxValue;
 
-  int pid;
+  const int pidCount;
+  int pidValues[4];
+  int* pids;
+
   int color;
+
+  float (*logic)(int*) = 0;
 
   virtual void addValue(float val);
   virtual void draw();
-  item(const char* name, int pid, int color) {
+  item(const char* name, int color, int pids[], int pidCount) : pidCount(pidCount) {
     active = true;
 
+    this->name = name;
     this->minValue = 0;
     this->maxValue = 0;
 
-    this->name = name;
-    this->pid = pid;
+    this->pids = pids;
+
     this->color = color;
+  }
+
+  void obdGetValues() {
+      
+    //get results of all pids
+    for (int i = 0; i < this->pidCount; i++) {
+  #ifdef OBD_ON
+      if (obd.readPID(this->pids[i], this->pidValues[i])) {
+        //TODO: oh no one failed what do we do?
+      }
+  #else
+      this->pidValues[i] = random(this->maxValue);
+  #endif
+    }
+    //do the transform?
+    if (this->logic) {
+      this->addValue(this->logic(this->pidValues));
+    } else {
+      this->addValue(this->pidValues[0]);
+    }
   }
 };
 
@@ -77,7 +105,7 @@ public:
   float values[DISPLAY_X];
   float lastRender[DISPLAY_X];
   
-  graphItem(const char* name, int pid, int color) : item(name, pid, color) {
+  graphItem(const char* name, int color, int pids[], int pidCount) : item(name, color, pids, pidCount) {
 
     this->average = 0;
     this->count = 0;
@@ -166,7 +194,7 @@ class barItem : public item {
 public:
   int height;
 
-  barItem(const char* name, int pid, int minValue, int maxValue, int color) : item(name, pid, color) {
+  barItem(const char* name, int minValue, int maxValue, int color, int pids[], int pidCount) : item(name, color, pids, pidCount) {
 
     this->height = 0;
     this->minValue = minValue;
@@ -210,17 +238,34 @@ void setup(void) {
   ts.setRotation(1);
 
   //config init
-  items[0] = new barItem("Throttle", PID_THROTTLE, 0, 100, ILI9341_RED);
+  items[0] = new barItem("Throttle", 0, 100, ILI9341_RED, new int{PID_THROTTLE}, 1);
   items[0]->maxValue = 100;
-  items[1] = new graphItem("RPM", PID_RPM, ILI9341_WHITE);
-  items[1]->maxValue = 9000;
+
+  items[1] = new graphItem("RPM", ILI9341_YELLOW, new int{PID_RPM}, 1);
+  //items[1]->maxValue = 9000;
   items[1]->active = true;
-  items[2] = new graphItem("Oil Temp", PID_ENGINE_OIL_TEMP, ILI9341_YELLOW);
-  items[2]->maxValue = 250;
+
+  items[2] = new graphItem("Oil Temp", ILI9341_YELLOW, new int{PID_ENGINE_OIL_TEMP}, 1);
+  //items[2]->maxValue = 250;
   items[2]->active = false;
-  items[3] = new graphItem("KMH", PID_SPEED, ILI9341_CYAN);
-  items[3]->maxValue = 150;
+
+  items[3] = new graphItem("MPH", ILI9341_YELLOW, new int{PID_SPEED}, 1);
+  //items[3]->maxValue = 150;
+  items[3]->logic = kmhToMPH;
   items[3]->active = false;
+
+  items[4] = new graphItem("MPG", ILI9341_YELLOW, new int[2]{PID_SPEED, PID_MAF_FLOW}, 2);
+  //items[4]->maxValue = 150;
+  items[4]->logic = calcMPG;
+  items[4]->active = false;
+   
+  items[5] = new graphItem("Fuel level", ILI9341_YELLOW, new int{PID_FUEL_LEVEL}, 1);
+  //items[5]->maxValue = 100;
+  items[5]->active = false;
+
+  items[6] = new graphItem("Fuel Usage", ILI9341_YELLOW, new int{PID_ENGINE_FUEL_RATE}, 1);
+  //items[6]->maxValue = 100;
+  items[6]->active = false;
 
   #ifdef OBD_ON
   //obd init 
@@ -243,12 +288,7 @@ void loop(void) {
 
     //collect values from obd
     for (int i = 0; i < ITEM_COUNT; i++) {
-  #ifdef OBD_ON
-      int val = obdGetValue(items[i]->pid);
-  #else
-      int val = random(items[i]->maxValue);
-  #endif
-      items[i]->addValue(val);
+      items[i]->obdGetValues();
     }
     
     tft.drawFastHLine(0, GRAPH_Y, DISPLAY_X + BAR_WIDTH, ILI9341_BLUE); // line between bottom section and graph
@@ -257,6 +297,9 @@ void loop(void) {
         items[i]->draw();
       }
     }
+    tft.setCursor(50, 8);
+    tft.print(dt);
+    tft.print(" ");
 
     //switch graph with touch
     timeSinceSwitch += dt;
